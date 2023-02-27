03ad3e99f4858343791db60decb303061677e02e
kah3vich <nik.b20@mail.ru>
2023-02-27 21:59:33 +0300

    feat: utrils github

--- utils/git.js
+++ utils/git.js
@@ -1,4 +1,4 @@
-for (let i = 3; i <= 11; i++) {
+for (let i = 0; i <= 1; i++) {
 	console.log(
 		`sed -i '1s/^/${i}\\n/' ./readme.md; git add .; git commit --amend --date='Wed Feb ${i} 14:00 2023 +0100' --no-edit; git push -f;`,
 	);
