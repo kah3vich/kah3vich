8aa9c0f0685c783d66b982e89a181ff39b8fdcbf
kah3vich <nik.b20@mail.ru>
2023-03-20 23:00:39 +0300

    feat: linux config & git config & config vs compiler & .vscode config

--- config/_compiler.py
+++ config/_compiler.py
@@ -29,24 +29,28 @@ def remove_first_last_lines(text):
 
     return result
 
-text = '{\n'
+def compiler_finally_config():
+    text = '{\n\t//| 🔥 Config Kah3vich - https://github.com/kah3vich \n\n\t//? 🧨 Info - https://code.visualstudio.com/docs/editor/codebasics \n\t//? 🤔 Config - https://code.visualstudio.com/docs/getstarted/settings \n'
 
-file_list = remove_elements_by_name(get_files_in_current_folder(), ['_info.json', '_.json', '_compiler.py'])
+    file_list = remove_elements_by_name(get_files_in_current_folder(), ['_info.txt', '_.json', '_compiler.py', '_backup.json'])
 
-for file in file_list:
-    with open(file, 'r', encoding="utf8") as input_file:
-        if (file_list[-1] == file):
-            text += f'\n{remove_first_last_lines(input_file.read())}\n'
-        else:
-            file_text = f'\n{remove_first_last_lines(input_file.read())}\n'
-            pattern = r"(.*)(\n[^\n]*)$"
-            replacement = r"\1,\2"
+    for file in file_list:
+        with open(file, 'r', encoding="utf8") as input_file:
+            if (file_list[-1] == file):
+                text += f'\n{remove_first_last_lines(input_file.read())}\n'
+            else:
+                file_text = f'\n{remove_first_last_lines(input_file.read())}\n'
+                pattern = r"(.*)(\n[^\n]*)$"
+                replacement = r"\1,\2"
 
-            text += re.sub(pattern, replacement, file_text)
+                text += re.sub(pattern, replacement, file_text)
 
-text += '\n}\n\n//| 🔥 by kah3vich 🔥\n'
+    text += '\n}\n\n//| 🔥 by kah3vich 🔥\n'
 
-with open('./_.json', 'w', encoding="utf-8") as output_file:
-    output_file.write(text)
+    with open('./_.json', 'w', encoding="utf-8") as output_file:
+        output_file.write(text)
 
-print('Done ✅')
+if __name__ == "__main__":
+    compiler_finally_config()
+
+    print('\ncompiler_finally_config - Done ✅')
