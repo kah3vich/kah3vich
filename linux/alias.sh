260672a5fd8ad3617f2f20fd8b5568722ff691e7
Nikita Zolkin <ni.zolkin@mish.design>
2024-04-29 13:41:56 +0300

    feat: vim config

--- linux/alias.sh
+++ linux/alias.sh
@@ -1,4 +1,4 @@
-# unalias -m '*' # Delete all aliases
+# unalias -m '*' # delete all aliases
 
 alias -- -='cd -'
 alias -g ...=../..
@@ -7,9 +7,8 @@ alias -g .....=../../../..
 alias -g ......=../../../../..
 
 alias c='clear'
-alias b='cd ../'
 alias rf='rm -rf'
 alias rz='exec zsh'
-alias conf='code ~/zshrc'
+alias conf='code ~/.zshrc'
 alias ls='ls --color=auto -al'
 alias s='cd ~; clear; exec zsh'
---
+++ vim/readme.md
@@ -0,0 +1,8 @@
+[Back](../)
+
+<div align="center">
+    <br />
+    <h1 align="center">Vim</h1>
+</div>
+
+...
